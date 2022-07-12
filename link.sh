0448237adf2ef436037dc9f6e9d5c0a8afb1a74d
Michael <mboudreaux123@gmail.com>
2022-07-12 14:37:17 -0500

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-for f in ./*; do
-    echo "$f"
-done
+
+link()
+(
+    cd "$1"
+    for i in ./*; do
+        if [ -d "$i" ]; then
+            echo "Dir: $i"
+            link "$i"
+        elif [ -f "$i" ]; then
+            echo "File: $1/$i"
+        fi
+    done
+)
+
+link
